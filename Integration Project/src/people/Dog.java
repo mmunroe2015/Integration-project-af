8c87eed697a2c3641bbf29443024bc378e082e9c
mmunr_000 <mmunr_000@10.100.216.125>
2018-01-25 18:42:49 -0500

    First Commit

---
+++ Integration Project/src/people/Dog.java
@@ -0,0 +1,49 @@
+//Mikayla Munroe
+package people;
+
+import java.util.Scanner;
+
+public class Dog {
+	double size;
+	String breed;
+	//main method
+
+	public static void main(String[] args) {
+	
+	Dog taz = new Dog();
+	Dog daisy = new Dog();
+	taz.size = 20;
+	daisy.size = 15;
+	taz.breed = "Dauschund";
+	daisy.breed = "Pug";
+	System.out.println(taz.breed);
+	
+	
+	
+	
+	}	
+	
+}
+	
+
+	
+
+
+
+
+
+	
+	
+	/*Definition of a variable: a space in memory for something to be used later
+	 * Variable names and descriptions
+	 *Byte: 8-bit. Minimum of -128 and max of 127
+	 *Short: 16 signed twos complement integer
+	 *Int: 32-bit type two's compliment
+	 *Long: 64-bit two's complement integer
+	 *Float: single-precision 32-bit IEEE 754 floating  point
+	 *Double: double-precision 64-bit IEEE 754 floating point
+	 *Boolean: only has two values, true and false
+	 *Char: 16-bit Unicode character
+	 *String:sequence of characters, is an object */  
+
+
---
+++ Integration Project/src/people/Person2.java
@@ -0,0 +1,9 @@
+package people;
+
+public class Person2 {
+
+	public void yourOccupation(String job){
+		System.out.println("You are a " + job);
+		
+	}
+}
