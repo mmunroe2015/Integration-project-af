5c24eb976a5588ddf166030a687c6669c2113daf
mmunr_000 <mmunr_000@10.100.216.142>
2018-03-13 18:01:09 -0400

    main method

--- Integration Project/src/people/Circle.java
+++ Integration Project/src/people/Circle.java
@@ -1,17 +1,34 @@
+//Mikayla Munroe cirle class
 package people;
 
 public class Circle {
   private double radius;
-  
+  private int x, y;
   public void setRadius(double rad) {
     radius = rad;
   }
- public double getRadius() {
-   return radius;
-   
- }
- public double calculateArea() {
-   return Math.PI * Math.pow(radius, 2);
- }
+
+  public double getRadius() {
+    return radius;
+
+  }
+
+  public double calculateArea() {
+    return Math.PI * Math.pow(radius, 2);
+  }
+  public double getDiameter() {
+    return radius * 2;
+  }
+  public void setX(int x) {
+    this.x = x;
+  }
+  public int getX() {
+    return x;
+  }
+  public void setY(int y) {
+    this.y = y;
+  }
+  public int getY() {
+    return y;
+  }
 }
-  
